62b4f638cc12c990a535db73d1675cc40d9f2ba2
Asilbek <asilbeksh7379@gmail.com>
2025-11-24 17:49:37 +0500

    Uploading main page code

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(page_title="Phishing Detection App", layout="wide")
+
+st.title("📌 Phishing Website Detection – Streamlit App")
+
+st.write("""
+Welcome!  
+Use the sidebar to navigate between:
+- **Data Exploration**
+- **Preprocessing**
+- **Model Training**
+- **Inference & Evaluation**
+""")
